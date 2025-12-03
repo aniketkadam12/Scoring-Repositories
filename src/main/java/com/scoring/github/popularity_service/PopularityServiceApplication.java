7059055499d6e73b044ff8078f1852aa46751a7a
Aniket Kadam <88384695+aniketkadam12@users.noreply.github.com>
2025-12-03 10:48:39 +0100

    remove unecessary dependencies

--- src/main/java/com/scoring/github/popularity_service/PopularityServiceApplication.java
+++ src/main/java/com/scoring/github/popularity_service/PopularityServiceApplication.java
@@ -2,11 +2,9 @@ package com.scoring.github.popularity_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
-@EnableFeignClients
-
 public class PopularityServiceApplication {
 
 	public static void main(String[] args) {
